a30c19ac24528a91f208ef1d2794336a32860d6f
abinandan1 <abinandan2018@gmail.com>
2024-07-30 21:13:04 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import emailRouter from "./routes/email.js";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "send email api" });
+});
 
 app.use("/api/v1", emailRouter);
 
